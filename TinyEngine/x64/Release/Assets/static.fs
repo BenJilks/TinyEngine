3e3efc602c80fca0b558870dd738ef6d8f5d9823
BenJilks <benjyjilks@gmail.com>
2019-10-11 15:15:12 +0000

    Cross platform building

--- TinyEngine/x64/Release/Assets/static.fs
+++
@@ -1,8 +0,0 @@
-#version 400
-uniform sampler2D defuse;
-in vec2 p_texture;
-
-out vec4 FragColor;
-void main(void) {
-	FragColor = texture(defuse, p_texture);
-}
---
+++ include/TinyDisplay.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <string>
+#include "glad.h"
+#include <GLFW/glfw3.h>
+#include "TinyMaths.h"
+using namespace std;
+
+namespace TinyEngine
+{
+	/* Create and handle a GLFW window and inputs */
+	class TinyDisplay
+	{
+	public:
+		TinyDisplay(unsigned int width, unsigned int height, string title, bool is_fullscreen = false);
+
+		/* Initializes the display library */
+		static void Init();
+
+		/* Update the display */
+		void Update();
+
+		/* Sets the clear colour of the screen */
+		void SetClearColour(Colour colour);
+
+		/* Returns if a key is down */
+		bool IsKeyDown(unsigned int key);
+
+		/* Returns the mouses position as a 2D vector */
+		Vec2f GetMousePosition();
+
+		/* Returns if the window should be closed */
+		int ShouldWindowClose();
+
+		/* Getters */
+		unsigned int GetWidth() { return m_width; }
+		unsigned int GetHeight() { return m_height; }
+
+		/* Returns the resolution of the primary monitor */
+		static Vec2i GetMonitorSize();
+
+		void Close() { m_should_close = true; }
+	private:
+		GLFWwindow* m_window;
+		string m_title;
+		unsigned int m_width, m_height;
+		bool m_should_close;
+	};
+}
